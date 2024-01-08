0069ece3242b7c1ffd08dae0fe97bc8fccaace86
Tiago Faria <tfaria@deloitte.pt>
2024-01-08 16:49:56 +0000

    Add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,13 +4,13 @@ const forecast = (latitude, longitude, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=0e6a6632b6eef0768294ef463b5f8ffb&query=${latitude},${longitude}`
 
     request(url, {json: true}, (error, response, body) => {
-        const {weather_descriptions, temperature, feelslike} = body.current
+        const {weather_descriptions, temperature, feelslike, humidity} = body.current
         if (error) {
             callback('Unable to connect WeatherStack')
         } else if (body.error) {
             callback('Unable to find location')
         } else {
-            callback(undefined, `${weather_descriptions[0]}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees out.`)
+            callback(undefined, `${weather_descriptions[0]}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees out. Humidity at ${humidity}.`)
         }
     })
 }
